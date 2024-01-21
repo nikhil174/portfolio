b80582b03621388bafd24a77bc3c167bd66a7133
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-01-21 17:11:22 +0530

    style contact btn

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,9 +6,17 @@ import './contact.css';
 
 const Contact = () => {
   const form = useRef();
+  const [loading, setLoading] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
+
+    if (loading) {
+      return; // Prevent sending another request while the previous one is still in progress
+    }
+
+    setLoading(true);
+
     emailjs
       .sendForm('service_0opvkvl', 'template_ue45sh2', form.current, 'PnYTnrvTtZ3p_4-Kf')
       .then(
@@ -19,7 +27,10 @@ const Contact = () => {
         (error) => {
           notifyError('Error sending email. Please try again.');
         }
-      );
+      )
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   const notifySuccess = (message) => {
@@ -77,15 +88,10 @@ const Contact = () => {
               <textarea name="message" id="" cols="30" rows="10" className='contact__form-input' placeholder='Write your message'></textarea>
             </div>
 
-            <button className="button button--flex">
-              Send Message<svg
-                className="button__icon"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
+            <button className="button button--flex" type="submit" disabled={loading}>
+              {loading && <i className="bx bx-loader-alt bx-spin"></i>} <span className='contact_btn_Text'>Send Message</span>
+            </button>
+            {/* <button>
                 <path
                   d="M14.2199 21.9352C13.0399 21.9352 11.3699 21.1052 10.0499 17.1352L9.32988 14.9752L7.16988 14.2552C3.20988 12.9352 2.37988 11.2652 2.37988 10.0852C2.37988 8.91525 3.20988 7.23525 7.16988 5.90525L15.6599 3.07525C17.7799 2.36525 19.5499 2.57525 20.6399 3.65525C21.7299 4.73525 21.9399 6.51525 21.2299 8.63525L18.3999 17.1252C17.0699 21.1052 15.3999 21.9352 14.2199 21.9352ZM7.63988 7.33525C4.85988 8.26525 3.86988 9.36525 3.86988 10.0852C3.86988 10.8052 4.85988 11.9052 7.63988 12.8252L10.1599 13.6652C10.3799 13.7352 10.5599 13.9152 10.6299 14.1352L11.4699 16.6552C12.3899 19.4352 13.4999 20.4252 14.2199 20.4252C14.9399 20.4252 16.0399 19.4352 16.9699 16.6552L19.7999 8.16525C20.3099 6.62525 20.2199 5.36525 19.5699 4.71525C18.9199 4.06525 17.6599 3.98525 16.1299 4.49525L7.63988 7.33525Z"
                   fill="var(--container-color)"
@@ -95,7 +101,7 @@ const Contact = () => {
                   fill="var(--conta-color)"
                 ></path>
               </svg>
-            </button>
+            </button> */}
           </form>
         </div>
       </div>
